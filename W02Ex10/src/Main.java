069a4a6291003c6a6eb88bb4b7ecad34328e403f
garunol <thuandsbg1@gmail.com>
2024-09-16 15:08:55 +0700

    10

---
+++ W02Ex10/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+        public static void main(String[] args) {
+            // Input
+
+            // Setup
+            int sum = 0;
+            int count = 0;
+            int number = 0;
+
+            // Process
+            while (count < 10) {
+                if (number % 2 == 0) {
+                    sum += number;
+                    count++;
+                }
+                number++;
+            }
+            // Output
+            System.out.println("Tổng của 10 số chẵn đầu tiên là: " + sum);
+        }
+    }
