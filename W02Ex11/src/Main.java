10e4d9e0202f43bf0be505d22d949cb147dd278f
garunol <thuandsbg1@gmail.com>
2024-09-16 15:23:38 +0700

    Excercise 11 of Week 01

---
+++ W02Ex11/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Input
+        System.out.println("Chương trình in ra các số lẻ từ 1 đến 99.");
+
+        // Setup
+
+        // Process
+        System.out.println("Các số lẻ từ 1 đến 99 là:");
+        for (int i = 1; i < 100; i += 2) {
+            System.out.print(i + " ");
+        }
+
+        // Output
+        System.out.println();
+    }
+}
+
