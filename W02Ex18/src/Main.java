4fdcff14f6aee10e0606da7cf487bc03dbf75256
garunol <thuandsbg1@gmail.com>
2024-09-18 19:18:44 +0700

    Excercise 18 of Week 01

---
+++ W02Ex18/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Setup
+
+        // Process
+        System.out.println("Các bội số của 3 từ 300 đến 3 là:");
+        for (int i = 300; i >= 3; i--) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        // Output
+    }
+}
